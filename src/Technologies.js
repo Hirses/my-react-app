4b599b72a866a3ad735c9dae2b1264e74d6d41e8
Hirses <ilya.teterin.hirs@gmail.com>
2021-08-30 20:38:51 +0500

    feet: Добавил компоненты Header и Technologies

---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies;
